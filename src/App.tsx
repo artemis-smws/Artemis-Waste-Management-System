22f9ac8a74b5a010f053d8c025be3aaca120dea2
SchadenKai <kairustecsonpersonal@gmail.com>
2023-04-06 19:24:00 +0800

    landing page ini

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.scss'
-import LoginCard from './components/loginCard'
+import LandingPage from './components/landingPage'
 import Navbar from './components/navbar'
 
 export function App() {
   return (
     <div className='position-relative'>
       <Navbar />
-      <LoginCard />
+      <LandingPage />
     </div>
   )
 }
---
+++ src/components/landingPage.tsx
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+  return (
+    <section
+      style={{ height: "100vh", width: "100vw", zIndex: "-1" }}
+      className="d-flex align-items-center justify-content-evenly w-100"
+      id="home"
+    >
+      <div className="artemis-logo"></div>  
+    </section>
+  );
+}
