6497fc6f782ed7ba652edec71b17cf1daca16fa4
SchadenKai <kairustecsonpersonal@gmail.com>
2023-04-14 14:32:28 +0800

    organized routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.scss'
-import LandingPage from './pages/client/landingPage'
-import Navbar from './components/layout/navbar'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import Sidebar from './components/layout/sidebar'
+import "./App.scss";
+import LandingPage from "./pages/client/landingPage";
+import Navbar from "./components/layout/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/layout/sidebar";
+import Admin from "./admin";
 
 export function App() {
-
-  return(
-  <div>
-    <Sidebar/>
-  </div>
-  )
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
- 
---
+++ src/admin.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "./components/layout/sidebar";
+
+export default function Admin() {
+    return(
+        <div>
+            <Sidebar/>
+        </div>
+    )
+}
