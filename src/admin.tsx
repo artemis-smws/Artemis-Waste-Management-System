db6a4c57cf2630ccea212ade59e82aa8b911ee19
HaaakDog <fakernagne10@gmail.com>
2023-04-21 10:11:56 +0800

    auto commit

--- src/admin.tsx
+++ src/admin.tsx
@@ -1,9 +1,37 @@
-import Sidebar from "./components/layout/sidebar";
+import "./admin/admin.scss";
+
 
 export default function Admin() {
     return(
         <div>
-            <Sidebar/>
+
+            <div className="d-flex justify-content-evenly align-items-center" id="admin-login">
+                <div className="d-flex justify-content-center" id="login-logo">
+                    <img src="./assets/img/placeholder-icon.png" />
+                </div>
+                <div className="d-flex justify-content-center" id="login-card">
+                    <div className ="d-flex flex-column align-items-center justify-content-center">
+                        <input type="text" placeholder="Email" name="email"/>
+                        <input type="password" placeholder="Password" name="password"/>
+                        <button type="button" className="btn" id="lgn-btn">LOGIN</button>
+                    </div>
+                </div>
+            </div>
+            
+            <footer>
+                <ul className="d-flex justify-content-evenly"id="f-lists">
+                    <li className="d-flex">
+                        <img src="./assets/img/bsu-logo.png"/><h1>Batangas State University</h1>
+                    </li>
+                    <li className="d-flex">
+                        <h1>BatState-U General Service Office</h1>
+                    </li>
+                    <li className="d-flex">
+                        <img src="./assets/img/emu_logo.png"/><h1>BatState-U Environmental Management Unit</h1>
+                    </li>
+                </ul>
+            </footer>
+
         </div>
     )
 }
