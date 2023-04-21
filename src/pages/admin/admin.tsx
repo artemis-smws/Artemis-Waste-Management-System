2deba1444bfa722e1a3f0b5a43fb763cae1904a0
SchadenKai <kairustecsonpersonal@gmail.com>
2023-04-22 02:42:07 +0800

    added google analytics

---
+++ src/pages/admin/admin.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "../../components/layout/sidebar";
+
+export default function Admin() {
+    return(
+        <div className="d-flex w-100 h-100">
+            <Sidebar/>
+        </div>
+    )
+}
