40b0a418ac653d2586b2257de7efce301ef7d11a
HaaakDog <fakernagne10@gmail.com>
2023-05-07 14:14:17 +0800

    initial commit

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
+import Dashboard from '../../pages/admin/dashboardPage';
+
 
 function Sidebar(){
 
@@ -18,7 +20,7 @@ function Sidebar(){
                     <div className="d-flex align-items-start" id="sLists">
                         <ul className="nav flex-column nav-pills me-3 w-100" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                             <li className="d-flex">
-                                <img src="./assets/img/home.png" width="35px" height="35px"/><button className="nav-link w-100 d-flex align-content-center" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                                <img src="./assets/img/bsu-logo.png" width="35px" height="35px"/><button className="nav-link w-100 d-flex align-content-center" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
                             </li>
                             <li className="d-flex">
                                 <img src="./assets/img/maps.png" width="35px" height="35px"/><button className="nav-link w-100 d-flex align-content-center" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
