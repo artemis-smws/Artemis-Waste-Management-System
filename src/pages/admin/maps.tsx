3fdb98e38226a3a94431566daa66f0d4ae237e03
SchadenKai <kairustecsonpersonal@gmail.com>
2023-05-23 03:04:46 +0800

    merged fixed

--- src/pages/admin/maps.tsx
+++ src/pages/admin/maps.tsx
@@ -5,40 +5,40 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import DropdownSelectDate from "../../components/layout/dropdownDate";
 import SelectSchool from "../../components/layout/selectSchool";
-import DisplayLineChart from "../../components/charts/wasteGenerated";
-import { Link } from "react-router-dom";
+import WasteGenerated from "../../components/charts/wasteGenerated";
 
 // Define a custom icon for the marker
 const customIcon = L.divIcon({
   className: "round-button",
   html: '<button type="button" className="btn" id="trash-status">60%</button>',
 });
-
 const customIcon1 = L.divIcon({
   className: "round-button",
   html: '<button type="button" className="btn" id="trash-status1">20%</button>',
 });
-
 const customIcon2 = L.divIcon({
   className: "round-button",
   html: '<button type="button" className="btn" id="trash-status2">90%</button>',
 });
-
-export default function Maps() {
-
+interface Props {
+  handleDashboard?: () => void;
+  handleBin?: () => void;
+}
+export default function Maps({ handleDashboard, handleBin }: Props) {
+  const handleButtonClick = () => {
+    console.log("Button clicked!");
+  };
   function myFunction(): void {
     const popup = document.getElementById("myPopup");
     if (popup) {
       popup.classList.toggle("show");
     }
   }
-
   function handlePopupClick(
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ): void {
     event.stopPropagation();
   }
-
   return (
     <div>
       <MapContainer
@@ -48,97 +48,99 @@ export default function Maps() {
         zoomControl={false}
       >
         <div className="navbar" id="maps-navbar">
-          <Link to="/dashboard">
-            <img
-              src="./assets/img/artemis-favicon.webp"
-              width="45px"
-              height="44px"
-            />
-          </Link>
-
+          <img
+            src="./assets/img/artemis-favicon.webp"
+            width="45px"
+            height="44px"
+          />
           <div
             className="d-flex justify-content-around align-items-center"
             id="map-navbar-list"
           >
-            <Link to="/dashboard" className="nav-link mx-2">
+            <a onClick={handleDashboard} className="nav-link mx-2">
               Dashboard
-            </Link>
-            <Link to="/bin" className="nav-link mx-2">
+            </a>
+            <a onClick={handleBin} className="nav-link mx-2">
               Bin
-            </Link>
+            </a>
             <input type="text" placeholder="Search" id="search-location" />
           </div>
         </div>
-
         <TileLayer
           maxNativeZoom={19}
           maxZoom={20}
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         <Marker position={[13.78409, 121.07486]} icon={customIcon}>
           <Popup>
             <Popup className="custom-popup">
-              <div id="popup-content">
-                <div className="d-flex justify-content-between">
-                  <div id="trashbin-id">
-                    <h1>Trash bin:</h1>
-                    <h1>2123</h1>
+              <div
+                id="popup-content"
+                className="d-flex justify-content-around align-items-center"
+              >
+                <div id="popup-left">
+                  <div className="d-flex justify-content-around">
+                    <div id="trashbin-id">
+                      <h1>Trash bin:</h1>
+                      <h1>2123</h1>
+                    </div>
+                    <div id="trashbin-location">
+                      <h1>Canteen - Near Door</h1>
+                      <h1>X: 204 Y: 102</h1>
+                    </div>
                   </div>
-                  <div id="trashbin-location">
-                    <h1>Canteen - Near Door</h1>
-                    <h1>X: 204 Y: 102</h1>
-                  </div>
-                </div>
-                <div
-                  className="d-flex justify-content-around"
-                  id="trash-weight"
-                >
-                  <div className="d-flex align-items-center">
-                    <button
-                      type="button"
-                      className="btn d-flex justify-content-center align-items-center"
-                      id="trash-status"
-                    >
-                      60%
-                    </button>
-                    <h1
-                      style={{
-                        fontFamily: "Inria Sans",
-                        fontSize: "16px",
-                        margin: "0",
-                      }}
-                    >
-                      Trashbin almost full
-                    </h1>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <img
-                      src="./assets/img/weight.png"
-                      width="30px"
-                      height="30px"
-                    />
-                    <h1
-                      style={{
-                        fontFamily: "Inria Sans",
-                        fontSize: "16px",
-                        margin: "0",
-                      }}
-                    >
-                      (weight) 64kg
-                    </h1>
+
+                  <div
+                    className="d-flex justify-content-around"
+                    id="trash-weight"
+                  >
+                    <div className="d-flex align-items-center">
+                      <button
+                        type="button"
+                        className="btn d-flex justify-content-center align-items-center"
+                        id="trash-status"
+                      >
+                        60%
+                      </button>
+                      <h1
+                        style={{
+                          fontFamily: "Inria Sans",
+                          fontSize: "14px",
+                          margin: "0",
+                        }}
+                      >
+                        Trashbin almost full
+                      </h1>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <img
+                        src="./assets/img/weight.png"
+                        width="30px"
+                        height="30px"
+                      />
+                      <h1
+                        style={{
+                          fontFamily: "Inria Sans",
+                          fontSize: "14px",
+                          margin: "0",
+                        }}
+                      >
+                        (weight) 64kg
+                      </h1>
+                    </div>
                   </div>
                 </div>
 
-              <div>
-                <div id="trashbin-activity">
-                  <div>
-                    <h1>Trashbin Activity</h1>
-                  </div>
-                  <div className="d-flex justify-content-center">
-                    <div id="trashbin-activity-chart">
-                      <DisplayLineChart />
+                <div>
+                  <div id="trashbin-activity">
+                    <div>
+                      <h1>Trashbin Activity</h1>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <div id="trashbin-activity-chart">
+                        <WasteGenerated />
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -146,15 +148,12 @@ export default function Maps() {
             </Popup>
           </Popup>
         </Marker>
-
         <Marker position={[13.78428, 121.0743]} icon={customIcon1}>
           <Popup>Ano to?</Popup>
         </Marker>
-
         <Marker position={[13.78504, 121.07391]} icon={customIcon2}>
           <Popup>Halo</Popup>
         </Marker>
-
         <button type="button" className="btn" id="legend" onClick={myFunction}>
           Legend
           <span className="popuptext" id="myPopup" onClick={handlePopupClick}>
