16012330e5dd706541efcaa5123ee22c0c65ebf2
SchadenKai <kairustecsonpersonal@gmail.com>
2023-07-08 14:44:21 +0800

    initial commits

---
+++ src/pages/client/landingPage.tsx
@@ -0,0 +1,108 @@
+import "./landingPage.scss";
+import Navbar from "../../components/layout/navbar";
+import { Parallax, ParallaxLayer, ParallaxProps } from "@react-spring/parallax";
+import About from "./about";
+import { useRef } from "react";
+import Features from "./features";
+import Contact from "./contact";
+
+export default function LandingPage() {
+  const layer: React.CSSProperties = {
+    height: "1000px",
+  };
+
+  const styles: React.CSSProperties = {
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+
+  const ref: any = useRef();
+
+  return (
+    <>
+      {/* first page */}
+      <Parallax pages={4} style={{ top: "0", left: "0" }} ref={ref}>
+        <ParallaxLayer offset={0} speed={0.25} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/background.svg)",
+            }}
+            className="home_parallax_layer"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.1} factor={1} style={layer}>
+          <div className="home_parallax_layer">
+            <img
+              src="./assets/img/placeholder-icon.png"
+              id="logo"
+              style={{ marginBottom: "250px" }}
+            />
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/jungle1.webp)",
+            }}
+            className="home_parallax_layer"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/jungle2.webp)",
+            }}
+            className="home_parallax_layer"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.28} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/jungle3.webp)",
+            }}
+            className="home_parallax_layer"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.27} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/jungle4.webp)",
+            }}
+            className="home_parallax_layer"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={-0.25} speed={0} factor={1} style={layer}>
+          <div
+            style={{
+              ...styles,
+              backgroundImage: "url(./assets/img/jungle5.webp)",
+            }}
+            className="home_parallax_layer"
+          ></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0} style={layer}>
+          <Navbar
+            handleFeature={() => ref.current.scrollTo(2)}
+            handleAbout={() => ref.current.scrollTo(1)}
+            handleContact={() => ref.current.scrollTo(3)}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0}>
+          <About className="other_parallax_layer" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={0}>
+          <Features className="other_parallax_layer" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={3} speed={0}>
+          <Contact className="other_parallax_layer" />
+        </ParallaxLayer>
+      </Parallax>
+    </>
+  );
+}
---
+++ src/pages/client/newFeatures.tsx
@@ -0,0 +1,118 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import React, { useRef } from "react";
+import Card from "../../components/card";
+
+interface Props {
+  className: string;
+}
+
+export default function NewFeatures({ className }: Props) {
+  const sectionBootstrap =
+    "h-100 w-100 d-flex flex-column align-items-center justify-content-center";
+  const ref: any = useRef();
+
+  return (
+    <div id="features" className={className + " py-3"}>
+      <h1>Key Features</h1>
+      <Parallax pages={5} ref={ref} horizontal>
+        <ParallaxLayer offset={0}>
+          <section
+            onClick={() => {
+              ref.current.scrollTo(1);
+            }}
+            className={sectionBootstrap}
+          >
+            <Card
+              cardHeader="Website Admin Dashboard"
+              img="./dashboard-card-img.png"
+              className="featureCard mx-1"
+            >
+              The web admin dashboard is the central hub for managing and
+              monitoring all aspects of your waste management system. From
+              tracking waste production rate to monitoring ArteMIS Trash bins
+              status.
+            </Card>
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <section
+            onClick={() => {
+              ref.current.scrollTo(2);
+            }}
+            className={sectionBootstrap}
+          >
+            <Card
+              cardHeader="Real Time Mapping"
+              img="./map-card-img.png"
+              className="featureCard mx-1"
+            >
+              Look on where your ArteMIS is located in our Real-time maps.
+              Monitor your ArteMIS trash bin's status at the same time be
+              informed on it's activity
+            </Card>
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <section
+            onClick={() => {
+              ref.current.scrollTo(3);
+            }}
+            className={sectionBootstrap}
+          >
+            <Card
+              cardHeader="ArteMIS is now in Mobile!"
+              img="./mobile-card-img.png"
+              className="featureCard mx-1"
+            >
+              Access ArteMIS not only through the Web but also through the
+              access of your phones. Monitor and analyze ArteMIS with ease of
+              access. If you're a volunteer, easily send waste information and
+              get notification about the ArteMIS bin updates.
+              <button className="btn btn-success my-3">
+                Download ArteMIS - Mobile now!
+              </button>
+            </Card>
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={3}>
+          <section
+            onClick={() => {
+              ref.current.scrollTo(4);
+            }}
+            className={sectionBootstrap}
+          >
+            <Card
+              cardHeader="IoT ArteMIS trash bin"
+              img="./iot-card-img.png"
+              className="featureCard mx-1"
+            >
+              Throw your waste in a modern way. ArteMIS trash bin easily
+              monitors capacity of the bin and alerts the ArteMIS System about
+              its status in real-time. Track where your bin is placed through
+              its built-in GPS.
+            </Card>
+          </section>
+        </ParallaxLayer>
+        <ParallaxLayer offset={4}>
+          <section
+            onClick={() => {
+              ref.current.scrollTo(0);
+            }}
+            className={sectionBootstrap}
+          >
+            <Card
+              cardHeader="ArteMIS Website"
+              img="./web-card-img.png"
+              className="featureCard mx-1"
+            >
+              Access ArteMIS through your favorite browser. You can now also
+              monitor the status of the ArteMIS trash bins installed within the
+              environment. Get to monitor the status of your environment covered
+              by the ArteMIS ecosystem.
+            </Card>
+          </section>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
