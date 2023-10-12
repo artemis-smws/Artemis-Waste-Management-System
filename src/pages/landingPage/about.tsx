eb513c2da1b67d43c2c854afababcf46c2c6d5ad
SchadenKai <kairustecsonpersonal@gmail.com>
2023-10-12 13:43:24 +0800

    temp

--- src/pages/landingPage/about.tsx
+++ src/pages/landingPage/about.tsx
@@ -2,6 +2,7 @@ export default function About() {
   const style: React.CSSProperties = {
     display: "grid",
     gridTemplateColumns: "50% 25% 25%",
+    backgroundImage: "url(./assets/img/about-bg.png)",
     padding: "10px 10px"
   };
   return (
