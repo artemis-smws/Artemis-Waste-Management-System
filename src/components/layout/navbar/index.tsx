9011309de8cef6ff2a4fd29aa5d42d7bd0cf76ab
SchadenKai <kairustecsonpersonal@gmail.com>
2023-12-12 04:28:54 +0800

    changed: landing page -- to fix later

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,43 +1,35 @@
-import './index.scss'
-import {NavLink, Link} from 'react-router-dom'
+import "./index.scss";
+import { NavLink, Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { BoxArrowInRight, DoorOpen } from "react-bootstrap-icons";
 
 // linked to single page references using useRef
 interface Props {
-  handleFeature ?: () => void,
-  handleAbout ?: () => void,
-  handleContact ?: () => void,
+  handleFeature?: () => void;
+  handleAbout?: () => void;
+  handleContact?: () => void;
 }
 
-
-
-export default function Navbar({handleFeature, handleAbout, handleContact} : Props) {
+export default function Navbar_({
+  handleFeature,
+  handleAbout,
+  handleContact,
+}: Props) {
   return (
-    <nav className="d-flex fixed-top navbar navbar-expand-lg  justify-content-between px-5 pt-4">
-      <Link to='/' reloadDocument className="navbar-brand align-text-center">
-        ArteMIS
-      </Link>
-      <button className="navbar-toggler" type="button">
-        Menu
-      </button>
-      <div className="collapse navbar-collapse justify-content-end">
-        <div className="navbar-nav justify-content-between flex-wrap">
-          <a onClick={handleAbout} className="nav-link mx-2">
-            ABOUT
-          </a>
-          <a onClick={handleFeature} className="nav-link mx-2">
-            FEATURES
-          </a>
-          <a onClick={handleContact} className="nav-link mx-2">
-            CONTACTS
-          </a>
-          <NavLink  to='/login' className="nav-link mx-2">
-            MAP
-          </NavLink>
-          <NavLink to='/login' className="nav-link mx-2">
-            ANALYTICS
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary w-100 d-flex justify-content-between position-absolute"
+    >
+      <Navbar.Brand href="/">Artemis</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="d-flex justify-content-end gap-3 w-100">
+          <Nav.Link href="/login" className="bg-success px-3 rounded d-flex align-items-center gap-3">
+            <BoxArrowInRight />
+            Login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
