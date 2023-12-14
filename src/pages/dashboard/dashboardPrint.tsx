30f9f9ea0acdfae4d466cf34d1fbdbae3ef10c57
SchadenKai <kairustecsonpersonal@gmail.com>
2023-12-14 13:02:47 +0800

    fix: github actions

--- src/pages/dashboard/dashboardPrint.tsx
+++ src/pages/dashboard/dashboardPrint.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react'
 export default function DashboardPrint(){
     const navigate = useNavigate()
     useEffect(() => {
-        // window.print()
         navigate('/dashboard')
     }, [])
     const tableHeaders = [
