ac4c32ac4e4e8ea6200bfdf28d29924c52c64bd1
SchadenKai <kairustecsonpersonal@gmail.com>
2024-02-17 15:14:51 +0800

    fixed one utils

--- src/pages/dashboardPage/utils/calculateAverage.ts
+++ src/pages/dashboardPage/utils/calculateAverage.ts
@@ -1,9 +1,8 @@
-import fetchDataFactory from "../../../api/fetchDataFactory"
-import useFetchLatest from "../../../hooks/useFetchLatest"
+import useFetch from "../../../hooks/useFetch"
 import getBuidlingNames from "./getBuildingNames"
 
 export default async function calculateAverage(){
-    const data = await useFetchLatest()
+    const data = await useFetch("waste/latest", "latest")
     const buildingCount = getBuidlingNames(data).length
     const totalWeight = data[0].overall_weight
     return totalWeight / buildingCount
