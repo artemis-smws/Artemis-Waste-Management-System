a63b665f18f0d8bdefec9bcfc2f95af397d011c8
jeyefsiii <fakernagne10@gmail.com>
2024-02-22 17:48:24 +0800

    added data page

--- src/pages/addDataPage/index.tsx
+++ src/pages/addDataPage/index.tsx
@@ -1,10 +1,28 @@
 import Sidebar from "../../components/layout/sidebar";
+import DataTable from "./components/DataTable";
+import Header from "./components/Header";
+import { useState } from "react";
+import React from "react";
 
 export default function AddPage() {
+
+    const [isDeleteButtonVisible, setDeleteButtonVisible] = useState<boolean>(false);
+
     return (
         <div className="d-flex">
-            <Sidebar/>
-            <div className="vw-100 vh-100"></div>
+            <div>
+                <Sidebar/>
+            </div>
+            
+            <div className="w-100 vh-100">
+                <div>
+                    <Header isDeleteButtonVisible={isDeleteButtonVisible}/>
+                </div>
+                <div className="m-4">
+                    <DataTable toggleDeleteButtonVisibility={setDeleteButtonVisible}/>
+                </div>
+                
+            </div>
         </div>
     )
 }
