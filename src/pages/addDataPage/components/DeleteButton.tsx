858b09b37d3b5cafa2f0f8ff66d18ea9f560af2e
jeyefsiii <fakernagne10@gmail.com>
2024-02-27 23:59:21 +0800

    changes in delete button

--- src/pages/addDataPage/components/DeleteButton.tsx
+++ src/pages/addDataPage/components/DeleteButton.tsx
@@ -1,25 +1,29 @@
-import { useState } from "react"
-import DeleteMessage from "./DeleteNotice"
+import { useState } from "react";
+import DeleteMessage from "./DeleteNotice";
 
-const DeleteButton = () => {
-
-    const [notice, setNotice] = useState(false)
-
-    const showNotice = () => {
-        setNotice(true)
-    }
-    const closeNotice = () => {
-        setNotice(false)
-    }
-
-    return(
-        <div>
-            <button type="button" className="btn btn-danger" onClick={showNotice}>Delete</button>
-            <div>
-                {notice && <DeleteMessage submit={closeNotice}/>}
-            </div>
-        </div>
-    )
+interface DeleteButtonProps {
+    onClick: () => void;
 }
 
-export default DeleteButton
+const DeleteButton: React.FC<DeleteButtonProps> = ({ onClick }) => {
+    const [notice, setNotice] = useState(false);
+
+    const showNotice = () => {
+        setNotice(true);
+    };
+
+    const closeNotice = () => {
+        setNotice(false);
+    };
+
+    return (
+        <div>
+            <button type="button" className="btn btn-danger" onClick={() => {onClick(); showNotice();}}>Delete</button>
+            <div>
+                {notice && <DeleteMessage submit={closeNotice} />}
+            </div>
+        </div>
+    );
+};
+
+export default DeleteButton;
