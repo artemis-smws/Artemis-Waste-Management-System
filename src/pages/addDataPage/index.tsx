5e1c9e27af8a6ad3b154ef7237016f24412613af
jeyefsiii <fakernagne10@gmail.com>
2024-02-27 23:53:54 +0800

    changes in index

--- src/pages/addDataPage/index.tsx
+++ src/pages/addDataPage/index.tsx
@@ -1,28 +1,33 @@
 import Sidebar from "../../components/layout/sidebar";
-import DataTable from "./components/DataTable";
 import Header from "./components/Header";
 import { useState } from "react";
-import React from "react";
+import TrashTable from "./components/DataTable";
 
 export default function AddPage() {
 
     const [isDeleteButtonVisible, setDeleteButtonVisible] = useState<boolean>(false);
 
+    const handleDeleteButtonVisibility = (isVisible: boolean) => {
+        setDeleteButtonVisible(isVisible);
+    };
+
+    const handleDelete = () => {
+        
+    };
+
     return (
-        <div className="d-flex">
-            <div>
+        <div className="d-flex vh-100 w-100">
+            <div className="vh-100">
                 <Sidebar/>
-            </div>
-            
+            </div>   
             <div className="w-100 vh-100">
                 <div>
-                    <Header isDeleteButtonVisible={isDeleteButtonVisible}/>
+                    <Header isDeleteButtonVisible={isDeleteButtonVisible} handleDelete={handleDelete}/>
                 </div>
-                <div className="m-4">
-                    <DataTable toggleDeleteButtonVisibility={setDeleteButtonVisible}/>
+                <div>
+                   <TrashTable setIsDeleteButtonVisible={handleDeleteButtonVisibility}/> 
                 </div>
-                
             </div>
         </div>
-    )
+    );
 }
