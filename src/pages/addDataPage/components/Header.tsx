99bbd73cd3304e49bfcc52f9a297f077481206d7
jeyefsiii <fakernagne10@gmail.com>
2024-03-01 09:34:47 +0800

    nah

--- src/pages/addDataPage/components/Header.tsx
+++ src/pages/addDataPage/components/Header.tsx
@@ -1,4 +1,3 @@
-// Header.tsx
 import React from "react";
 import DeleteButton from "./DeleteButton";
 import AddWasteButton from "./AddButton";
@@ -6,30 +5,28 @@ import DropdownComponent from "./DropdownFilter";
 import CalendarButton from "./Calendar";
 
 interface HeaderProps {
-	isDeleteButtonVisible: boolean;
-	handleDelete: () => void;
+  isDeleteButtonVisible: boolean;
+  handleDelete: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({
-	isDeleteButtonVisible,
-	handleDelete,
+  isDeleteButtonVisible,
+  handleDelete,
 }) => {
-	return (
-		<div className="d-flex justify-content-between align-items-center border-bottom border-2 shadow p-3 w-100">
-			<div className="d-flex gap-3 justify-content-between align-items-center w-100">
-				<div className="w-50 d-flex gap-3">
-					<CalendarButton />
-					{isDeleteButtonVisible && (
-						<DeleteButton onClick={handleDelete} />
-					)}
-					<DropdownComponent />
-				</div>
-				<div>
-					<AddWasteButton />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="d-flex justify-content-between align-items-center border-bottom border-2 shadow p-3 w-100">
+      <div className="d-flex gap-3 justify-content-between align-items-center w-100">
+        <div className="w-50 d-flex gap-3">
+          <CalendarButton />
+          {isDeleteButtonVisible && <DeleteButton onClick={handleDelete} />}
+          <DropdownComponent />
+        </div>
+        <div>
+          <AddWasteButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
