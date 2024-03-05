b617d06981d1a408d4cbe115339e64c67137fdae
jeyefsiii <fakernagne10@gmail.com>
2024-03-05 09:45:42 +0800

    adding forms in add waste inputs

--- src/pages/addDataPage/components/AddWaste.tsx
+++ src/pages/addDataPage/components/AddWaste.tsx
@@ -3,21 +3,21 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MyDatePicker from "./DatePicker";
 import "../styles/AddWaste.scss";
-import { IoIosCloseCircleOutline } from "react-icons/io";
-import {
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Form,
-} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import { LuCalendarDays } from "react-icons/lu";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddWaste = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedBuilding, setSelectedBuilding] = useState("Select Building");
-  const [selectedType, setSelectedType] = useState("Select Type of Waste");
   const [wasteDescription, setWasteDescription] = useState("");
   const [weight, setWeight] = useState("");
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const target = e.target as HTMLInputElement;
+    setSelectedDate(new Date(target.value));
+  };
 
   const handleSubmit = () => {
     setIsOpen(false);
@@ -42,14 +42,6 @@ const AddWaste = () => {
     setIsOpen(false);
   };
 
-  const handleSelectBuilding = (item: string) => {
-    setSelectedBuilding(item);
-  };
-
-  const handleSelectType = (type: string) => {
-    setSelectedType(type);
-  };
-
   const handleWasteDescriptionChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -71,114 +63,82 @@ const AddWaste = () => {
             <div className="header w-100 p-3 d-flex justify-content-start align-items-center">
               <h1 className="m-0">Add Waste</h1>
             </div>
-            <div className="input-section w-100 h-100 p-4 d-flex flex-column justify-content-center">
-              <div className="d-flex justify-content-center align-items-center gap-3">
-                <div>
-                  <label>Building</label>
-                  <Dropdown>
-                    <DropdownToggle
-                      className="add-waste-input d-flex justify-content-between align-items-center"
-                      variant="light"
-                    >
-                      {selectedBuilding}
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem
-                        onClick={() => handleSelectBuilding("CEAFA BUILDING")}
-                      >
-                        CEAFA BUILDING
-                      </DropdownItem>
-                      <DropdownItem
-                        onClick={() => handleSelectBuilding("CICS BUILDING")}
-                      >
-                        CICS BUILDING
-                      </DropdownItem>
-                      <DropdownItem
-                        onClick={() => handleSelectBuilding("CIT BUILDING")}
-                      >
-                        CIT BUILDING
-                      </DropdownItem>
-                      <DropdownItem
-                        onClick={() => handleSelectBuilding("ACES BUILDING")}
-                      >
-                        ACES BUILDING
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
+            <Form className="input-section w-100 h-100 d-flex flex-column justify-content-around">
+              <div>
+                <div className="d-flex justify-content-center align-items-center gap-3">
+                  <div>
+                    <Form.Label>Building</Form.Label>
+                    <Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
+                      <option value="">CEAFA BUILDING</option>
+                      <option value="">CICS BUILDING</option>
+                      <option value="">CIT BUILDING</option>
+                      <option value="">ACES BUILDING</option>
+                    </Form.Select>
+                  </div>
+                  <div>
+                    <Form.Label>Date</Form.Label>
+                    <div className="row">
+                      <div className="col">
+                        <div className="date-picker-wrapper">
+                          <Form.Control
+                            as={DatePicker}
+                            selected={selectedDate}
+                            onChange={handleChange}
+                            className="form-control"
+                          />
+                          <span className="date-picker-icon">
+                            <LuCalendarDays />
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <label>Date</label>
-                  <MyDatePicker />
+                <div className="d-flex justify-content-start flex-column mt-3 px-3">
+                  <Form.Label>Type of Waste</Form.Label>
+                  <Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
+                    <option value="">BIODEGRADABLE</option>
+                    <option value="">RECYCLABLE</option>
+                    <option value="">FOOD WASTE</option>
+                    <option value="">INFECTIOUS</option>
+                  </Form.Select>
+                  <div className="mt-3">
+                    <Form.Label>Waste Description</Form.Label>
+                    <Form.Control
+                      type="text"
+                      className="add-waste-input"
+                      value={wasteDescription}
+                      onChange={handleWasteDescriptionChange}
+                    ></Form.Control>
+                  </div>
+                  <div className="mt-3">
+                    <Form.Label>Weight</Form.Label>
+                    <Form.Control
+                      type="number"
+                      className="weight-input"
+                      value={weight}
+                      onChange={handleWeightChange}
+                    ></Form.Control>
+                  </div>
                 </div>
               </div>
-              <div className="d-flex justify-content-start flex-column mt-3">
-                <label>Type of Waste</label>
-                <Dropdown>
-                  <DropdownToggle
-                    className="add-waste-input d-flex justify-content-between align-items-center"
-                    variant="light"
-                  >
-                    {selectedType}
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem
-                      onClick={() => handleSelectType("BIODEGRADABLE")}
-                    >
-                      BIODEGRADABLE
-                    </DropdownItem>
-                    <DropdownItem
-                      onClick={() => handleSelectType("RECYCLABLE")}
-                    >
-                      RECYCLABLE
-                    </DropdownItem>
-                    <DropdownItem
-                      onClick={() => handleSelectType("FOOD WASTE")}
-                    >
-                      FOOD WASTE
-                    </DropdownItem>
-                    <DropdownItem
-                      onClick={() => handleSelectType("INFECTIOUS")}
-                    >
-                      INFECTIOUS
-                    </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-                <Form className="mt-3">
-                  <label>Waste Description</label>
-                  <Form.Control
-                    type="text"
-                    className="add-waste-input"
-                    value={wasteDescription}
-                    onChange={handleWasteDescriptionChange}
-                  ></Form.Control>
-                </Form>
-                <Form className="mt-3">
-                  <label>Weight</label>
-                  <Form.Control
-                    type="number"
-                    className="weight-input"
-                    value={weight}
-                    onChange={handleWeightChange}
-                  ></Form.Control>
-                </Form>
+              <div className="d-flex justify-content-end align-items-center w-100 px-3">
+                <Button
+                  type="button"
+                  className="btn btn-secondary m-1"
+                  onClick={handleClose}
+                >
+                  Close
+                </Button>
+                <Button
+                  type="submit"
+                  className="btn btn-success m-1"
+                  onClick={handleSubmit}
+                >
+                  Confirm
+                </Button>
               </div>
-            </div>
-            <div className="footer d-flex justify-content-end align-items-center w-100 p-3">
-              <button
-                type="button"
-                className="btn btn-secondary m-1"
-                onClick={handleClose}
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                className="btn btn-success m-1"
-                onClick={handleSubmit}
-              >
-                Confirm
-              </button>
-            </div>
+            </Form>
           </div>
         </div>
       )}
--- src/pages/addDataPage/components/DatePicker.tsx
+++ src/pages/addDataPage/components/DatePicker.tsx
@@ -3,19 +3,22 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import "../styles/AddWaste.scss";
 import { LuCalendarDays } from "react-icons/lu";
+import { Form } from "react-bootstrap";
 
 const MyDatePicker: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const handleChange = (date: Date | null) => {
-    setSelectedDate(date);
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const target = e.target as HTMLInputElement;
+    setSelectedDate(new Date(target.value));
   };
 
   return (
     <div className="row">
       <div className="col">
         <div className="date-picker-wrapper">
-          <DatePicker
+          <Form.Control
+            as={DatePicker}
             selected={selectedDate}
             onChange={handleChange}
             className="form-control"
