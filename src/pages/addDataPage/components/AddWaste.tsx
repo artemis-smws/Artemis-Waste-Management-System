ffae2efdbffee7ab3d463c6b43770f105645ff86
SchadenKai <kairustecsonpersonal@gmail.com>
2024-03-07 00:06:56 +0800

    connected data table to server

--- src/pages/addDataPage/components/AddWaste.tsx
+++ src/pages/addDataPage/components/AddWaste.tsx
@@ -3,187 +3,146 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MyDatePicker from "./DatePicker";
 import "../styles/AddWaste.scss";
-import {
-	Form,
-	Button,
-	Dropdown,
-	DropdownToggle,
-	DropdownMenu,
-	DropdownItem,
-} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import { LuCalendarDays } from "react-icons/lu";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const AddWaste = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [wasteDescription, setWasteDescription] = useState("");
-	const [weight, setWeight] = useState("");
-	const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-		const target = e.target as HTMLInputElement;
-		setSelectedDate(new Date(target.value));
-	};
 
-	const handleSubmit = () => {
-		setIsOpen(false);
-		// Show success toast
-		toast.success("Data Added. ", {
-			position: "bottom-right",
-			autoClose: 5000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			draggable: true,
-			progress: undefined,
-			theme: "dark",
-		});
-	};
+  const [weight, setWeight] = useState("");
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-	const handleOpen = () => {
-		setIsOpen(true);
-	};
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const target = e.target as HTMLInputElement;
+    setSelectedDate(new Date(target.value));
+  };
 
-	const handleClose = () => {
-		setIsOpen(false);
-	};
+  const handleSubmit = () => {
+    setIsOpen(false);
+    // Show success toast
+    toast.success("Data Added. ", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
 
-	const handleWasteDescriptionChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setWasteDescription(event.target.value);
-	};
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
 
-	const handleWeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setWeight(event.target.value);
-	};
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
-	function handleSelectType(arg0: string): void {
-		throw new Error("Function not implemented.");
-	}
+  const handleWeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(event.target.value);
+  };
 
-	return (
-		<>
-			<button
-				type="button"
-				className="btn btn-success"
-				onClick={handleOpen}
-			>
-				Add Waste
-			</button>
-			{isOpen && (
-				<div className="add-card">
-					<div className="add-card-content d-flex flex-column justify-content-between align-items-center">
-						<div className="header w-100 p-3 d-flex justify-content-start align-items-center">
-							<h1 className="m-0">Add Waste</h1>
-						</div>
-						<Form className="input-section w-100 h-100 d-flex flex-column justify-content-around px-3">
-							<div>
-								<div className="d-flex justify-content-center align-items-center gap-3 w-100">
-									<div>
-										<Form.Label>Building</Form.Label>
-										<Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
-											<option value="">
-												CEAFA BUILDING
-											</option> 
-											<option value="">
-												CICS BUILDING
-											</option>
-											<option value="">
-												CIT BUILDING
-											</option>
-											<option value="">
-												ACES BUILDING
-											</option>
-										</Form.Select>
-									</div>
-									<div>
-										<Form.Label>Date</Form.Label>
-										<div className="row">
-											<div className="col">
-												<div className="date-picker-wrapper">
-													<Form.Control
-														as={DatePicker}
-														selected={selectedDate}
-														onChange={handleChange}
-														className="form-control"
-													/>
-													<span className="date-picker-icon">
-														<LuCalendarDays />
-													</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div className="d-flex justify-content-start flex-column mt-3">
-									<Form.Label>Type of Waste</Form.Label>
-									<Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
-										<option value="">BIODEGRADABLE</option>
-										<option value="">RECYCLABLE</option>
-										<option value="">FOOD WASTE</option>
-										<option value="">INFECTIOUS</option>
-									</Form.Select>
-									<div className="mt-3">
-										<Form.Label>
-											Waste Description
-										</Form.Label>
-										<Form.Control
-											type="text"
-											className="add-waste-input"
-											value={wasteDescription}
-											onChange={
-												handleWasteDescriptionChange
-											}
-										></Form.Control>
-									</div>
-									<div className="mt-3">
-										<Form.Label>Weight</Form.Label>
-										<Form.Control
-											type="number"
-											className="weight-input"
-											value={weight}
-											onChange={handleWeightChange}
-										></Form.Control>
-									</div>
-								</div>
-							</div>
-							<div className="d-flex justify-content-end align-items-center w-100">
-								<Button
-									type="button"
-									className="btn btn-secondary m-1"
-									onClick={handleClose}
-								>
-									Close
-								</Button>
-								<Button
-									type="submit"
-									className="btn btn-success m-1"
-									onClick={handleSubmit}
-								>
-									Confirm
-								</Button>
-							</div>
-						</Form>
-					</div>
-				</div>
-			)}
+  return (
+    <>
+      <button type="button" className="btn btn-success" onClick={handleOpen}>
+        Add Waste
+      </button>
+      {isOpen && (
+        <div className="add-card">
+          <div className="add-card-content d-flex flex-column justify-content-between align-items-center">
+            <div className="header w-100 p-3 d-flex justify-content-start align-items-center">
+              <h1 className="m-0">Add Waste</h1>
+            </div>
+            <Form className="input-section w-100 h-100 d-flex flex-column justify-content-around px-3">
+              <div>
+                <div className="d-flex justify-content-center align-items-center gap-3">
+                  <div>
+                    <Form.Label>Building</Form.Label>
+                    <Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
+                      <option value="CEAFA">CEAFA Building</option>
+                      <option value="CICS">CICS Building</option>
+                      <option value="CIT">CIT Building</option>
+                      <option value="ACES">ACES Building</option>
+                    </Form.Select>
+                  </div>
+                  <div>
+                    <Form.Label>Date</Form.Label>
+                    <div className="row">
+                      <div className="col">
+                        <div className="date-picker-wrapper">
+                          <Form.Control
+                            as={DatePicker}
+                            selected={selectedDate}
+                            onChange={handleChange}
+                            className="form-control"
+                          />
+                          <span className="date-picker-icon">
+                            <LuCalendarDays />
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-start flex-column mt-3">
+                  <Form.Label>Type of Waste</Form.Label>
+                  <Form.Select className="add-waste-input d-flex justify-content-between align-items-center">
+                    <option value="">Biodegredable</option>
+                    <option value="">Recyclable</option>
+                    <option value="">Food Waste</option>
+                    <option value="">Infectious</option>
+                  </Form.Select>
+                  <div className="mt-3">
+                    <Form.Label>Weight</Form.Label>
+                    <Form.Control
+                      type="number"
+                      className="weight-input"
+                      value={weight}
+                      onChange={handleWeightChange}
+                    ></Form.Control>
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex justify-content-end align-items-center w-100">
+                <Button
+                  type="button"
+                  className="btn btn-secondary m-1"
+                  onClick={handleClose}
+                >
+                  Close
+                </Button>
+                <Button
+                  type="submit"
+                  className="btn btn-success m-1"
+                  onClick={handleSubmit}
+                >
+                  Confirm
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      )}
 
-			<ToastContainer
-				position="bottom-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme="dark"
-			/>
-		</>
-	);
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+  );
 };
 
 export default AddWaste;
